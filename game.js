d681300f8c5ba9df8720a17b2fa5ed24ba0e72d0
Dame33 <damonlam315@gmail.com>
2023-12-05 19:47:49 -0500

    Update game.js

--- game.js
+++ game.js
@@ -31,12 +31,6 @@ function startGame() {
     // Subtract the initial bet from the balance
     balance -= bet;
 
-    // Check if the balance is less than the bet or less than 0
-    if (balance < bet || balance < 0) {
-        alert('$100 has been gifted.');
-        balance = 100;
-    }
-
     // Get random values for player and dealer
     var player1 = getRandomNumber();
     var player2 = getRandomNumber();
@@ -61,12 +55,20 @@ function startGame() {
         balance += 2 * bet; // Adjust the balance
         resultMessage = `Player wins: $${2 * bet}`;
         displayWinnerMessage('Player wins!');
+        displayWinnerImage('https://pbs.twimg.com/ext_tw_video_thumb/1543212053036679174/pu/img/b6n1jUriItaJOiAd.jpg:large');
     } else if (playerTotal < dealerTotal) {
         resultMessage = `Player loses: $${bet}`;
         displayWinnerMessage('Dealer wins!');
+        displayWinnerImage('https://media4.giphy.com/media/JudOfDOQTmIs5Z0PU7/giphy.gif');
     } else {
         resultMessage = `It's a tie. No money lost or won.`;
         displayWinnerMessage('It\'s a tie!');
+    }
+
+    // Check if the balance is less than or equal to 0
+    if (balance <= 0) {
+        alert('$100 has been gifted.');
+        balance = 100;
     }
 
     // Save the updated balance to localStorage
@@ -100,6 +102,15 @@ function displayWinnerMessage(winnerMessage) {
     winnerElement.style.fontSize = '24px';
     winnerElement.innerText = winnerMessage;
     messageContainer.appendChild(winnerElement);
+}
+
+// Function to display the winner image
+function displayWinnerImage(imageURL) {
+    var imageElement = document.createElement('img');
+    imageElement.src = imageURL;
+    imageElement.style.width = '100px'; // Adjust the size as needed
+    imageElement.style.marginTop = '10px';
+    messageContainer.appendChild(imageElement);
 }
 
 // Function to add "Press to Play Again" button
