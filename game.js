1753b18148ae4d92e6721d416c13becc41e9757f
Dame33 <damonlam315@gmail.com>
2023-12-05 19:22:30 -0500

    Update game.js

--- game.js
+++ game.js
@@ -3,19 +3,25 @@ import { getRandomNumber } from './random.js';
 // Retrieve the balance from localStorage or set it to the starting balance (100)
 var balance = parseInt(localStorage.getItem('balance')) || 100;
 
-function startGame() {
-    // Clear previous messages
-    var welcomeContainer = document.getElementById('welcomeContainer');
-    welcomeContainer.innerHTML = '';
+// Create a message container at the bottom of the screen
+var messageContainer = document.createElement('div');
+messageContainer.style.position = 'fixed';
+messageContainer.style.bottom = '0';
+messageContainer.style.left = '0';
+messageContainer.style.width = '100%';
+messageContainer.style.backgroundColor = '#333';
+messageContainer.style.color = '#fff';
+messageContainer.style.padding = '10px';
+document.body.appendChild(messageContainer);
 
+function startGame() {
     // Get the bet from the user
     var betString = prompt(`Your current balance: $${balance}\nEnter your bet:`);
 
     // Validate the bet
     var bet = parseInt(betString);
     if (isNaN(bet) || bet <= 0 || bet > balance) {
-        alert('Invalid bet. Please enter a valid bet.');
-        startGame(); // Restart the game if the bet is invalid
+        displayMessage('Invalid bet. Please enter a valid bet.');
         return;
     }
 
@@ -44,11 +50,14 @@ function startGame() {
     if (playerTotal > dealerTotal) {
         balance += 2 * bet; // Adjust the balance
         resultMessage = `Player wins: $${2 * bet}`;
+        displayWinnerMessage('Player wins!');
     } else if (playerTotal < dealerTotal) {
         balance -= bet; // Adjust the balance
         resultMessage = `Player loses: $${bet}`;
+        displayWinnerMessage('Dealer wins!');
     } else {
         resultMessage = `It's a tie. No money lost or won.`;
+        displayWinnerMessage('It\'s a tie!');
     }
 
     // Save the updated balance to localStorage
@@ -59,9 +68,6 @@ function startGame() {
 
     // Display the updated balance
     displayMessage(`Balance: $${balance}`, true);
-
-    // Display the winner message
-    displayWinnerMessage(playerTotal, dealerTotal);
 
     // Check if the balance allows for another round
     if (balance > 0 && confirm('Do you want to play another round?')) {
@@ -81,21 +87,15 @@ function displayMessage(message, lineBreak) {
         messageElement.style.marginBottom = '20px'; // Add some space between messages
     }
 
-    document.body.appendChild(messageElement);
+    messageContainer.appendChild(messageElement);
 }
 
 // Function to display the winner message
-function displayWinnerMessage(playerTotal, dealerTotal) {
-    var winnerMessage = '';
-    if (playerTotal > dealerTotal) {
-        winnerMessage = 'Player wins!';
-    } else if (playerTotal < dealerTotal) {
-        winnerMessage = 'Dealer wins!';
-    } else {
-        winnerMessage = 'It\'s a tie!';
-    }
-
-    displayMessage(winnerMessage, true);
+function displayWinnerMessage(winnerMessage) {
+    var winnerElement = document.createElement('p');
+    winnerElement.style.fontSize = '24px';
+    winnerElement.innerText = winnerMessage;
+    messageContainer.appendChild(winnerElement);
 }
 
 export { startGame };
